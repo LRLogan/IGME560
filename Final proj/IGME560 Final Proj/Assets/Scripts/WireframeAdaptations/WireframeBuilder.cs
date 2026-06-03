8aa676364edae5e1de7f76071a6ee49b73dacca4
LRLogan <lrl2777@g.rit.edu>
2026-06-03 18:49:18 -0400

    Solid progress on GPU instacing
    Just have to re figure out centering

--- Final proj/IGME560 Final Proj/Assets/Scripts/WireframeAdaptations/WireframeBuilder.cs
+++ Final proj/IGME560 Final Proj/Assets/Scripts/WireframeAdaptations/WireframeBuilder.cs
@@ -1,26 +1,54 @@
+using NUnit;
+using System.Collections.Generic;
 using System.Net;
 using Unity.VisualScripting;
 using UnityEditor.Networking.PlayerConnection;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+public struct Edge
+{
+    public Vector3 start;
+    public Vector3 end;
+
+    public Edge(Vector3 start, Vector3 end)
+    {
+        this.start = start;
+        this.end = end;
+    }
+}
+
 /// <summary>
 /// Wireframe visualizer of a heightmap written by Logan Larrondo
 /// </summary>
 public class WireframeBuilder : MonoBehaviour
 {
+    // General vars
     [SerializeField] private TerrainSettings settings;
     [SerializeField] [Tooltip("1 for a 1:1")] private int pointToVertRatio;
+    [SerializeField] private bool useVEB = true;
+
+    // Game object oriented pipeline
+    private GameObject[,] vertHeightMap;
     [SerializeField] private GameObject sphere;
-    [SerializeField] private GameObject cyl;
+    [SerializeField] private GameObject cyl; 
+
+    // Parent organize
     [SerializeField] private Transform vertParent;
     [SerializeField] private Transform edgeParent;
     [SerializeField] private Transform wireframeParent;
-    private GameObject[,] vertHeightMap;
 
+    // Optional camera vars
     [Header("Custom center settings. Disreguard if using other centerpoint")]
     [SerializeField] private float camOffsetZ = 75;
     [SerializeField] private GameObject cam;
+
+    // Vertex Edge Buffer oriented pipeline
+    private Vector3[,] vertexMap;
+    private List<Edge> edges = new();
+    private List<Matrix4x4> edgeMatrices = new();
+    [SerializeField] private Material cylMat;
+    [SerializeField] private Mesh cylMesh;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -35,25 +63,35 @@ public class WireframeBuilder : MonoBehaviour
          * The reason I am building a sequence and some other seemingly hard coded values
          * is to have a fine control over the final animation as it will be exported
          */
-        StartFullWireframePipeline();
+
+        if (!useVEB) StartFullWFPipeline_GO();
+        else StartFullWFPipeline_VEB();
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if(useVEB)RenderWireframe();
     }
 
     /// <summary>
     /// Entry point for full pipeline
     /// </summary>
-    public void StartFullWireframePipeline()
+    public void StartFullWFPipeline_GO()
     {
         TerrainPointData[,] heightmap = TerrainGen.GenerateTerrainHeightMap(settings);
-        BuildWireframeFromHeights(heightmap);
+        BuildWireframeFromHeights_GO(heightmap);
         UseHMCenter(heightmap);
     }
 
+    public void StartFullWFPipeline_VEB()
+    {
+        TerrainPointData[,] heightmap = TerrainGen.GenerateTerrainHeightMap(settings);
+        BuildWireframeFromHeights_VEB(heightmap);
+        UseHMCenter_VEB(heightmap);
+    }
+
+    #region General Helpers
     public void UseHMCenter(TerrainPointData[,] heightmap)
     {
         // Center the display on the screen 
@@ -61,12 +99,37 @@ public class WireframeBuilder : MonoBehaviour
         cam.transform.position = new Vector3(heightmap.GetLength(0) * 1.5f, heightmap.GetLength(0) / 2, -heightmap.GetLength(1) - camOffsetZ);
     }
 
+    public void UseHMCenter_VEB(TerrainPointData[,] heightmap)
+    {
+        Vector3 center = Vector3.zero;
+
+        foreach (Vector3 v in vertexMap)
+        {
+            center += v;
+        }
+
+        center /= vertexMap.Length;
+
+        for (int z = 0; z < vertexMap.GetLength(1); z++)
+        {
+            for (int x = 0; x < vertexMap.GetLength(0); x++)
+            {
+                vertexMap[x, z] -= center;
+            }
+        }
+
+        cam.transform.position = new Vector3(heightmap.GetLength(0) * 1.5f, heightmap.GetLength(0) / 2, -heightmap.GetLength(1) - camOffsetZ);
+    }
+    #endregion
+
+    #region Game Object oriented pipeline
+
     /// <summary>
     /// Entry point for the specific generation of the mesh
     /// </summary>
     /// <param name="heightmap"></param>
     /// <returns></returns>
-    public Transform BuildWireframeFromHeights(TerrainPointData[,] heightmap)
+    public Transform BuildWireframeFromHeights_GO(TerrainPointData[,] heightmap)
     {
         // Basic height map set up
         int vertWidth = 
@@ -120,7 +183,7 @@ public class WireframeBuilder : MonoBehaviour
 
                 if (x + 1 < width)
                 {
-                    ConnectPoints(
+                    ConnectPoints_GO(
                         current,
                         vertHeightMap[x + 1, z].gameObject
                     );
@@ -128,7 +191,7 @@ public class WireframeBuilder : MonoBehaviour
 
                 if (z + 1 < height)
                 {
-                    ConnectPoints(
+                    ConnectPoints_GO(
                         current,
                         vertHeightMap[x, z + 1].gameObject
                     );
@@ -137,7 +200,7 @@ public class WireframeBuilder : MonoBehaviour
                 if (x + 1 < width &&
                     z + 1 < height)
                 {
-                    ConnectPoints(
+                    ConnectPoints_GO(
                         current,
                         vertHeightMap[x + 1, z + 1].gameObject
                     );
@@ -152,7 +215,7 @@ public class WireframeBuilder : MonoBehaviour
     /// </summary>
     /// <param name="go1"></param>
     /// <param name="go2"></param>
-    private void ConnectPoints(GameObject go1, GameObject go2)
+    private void ConnectPoints_GO(GameObject go1, GameObject go2)
     {
         GameObject tempCyl = Instantiate(cyl, edgeParent);
 
@@ -174,4 +237,164 @@ public class WireframeBuilder : MonoBehaviour
             (go1.transform.position + go2.transform.position) * 0.5f;
 
     }
+    #endregion
+
+    #region Vertex Edge Buffer oriented pipeline
+
+    /// <summary>
+    /// Entry point for the specific generation of the mesh
+    /// </summary>
+    /// <param name="heightmap"></param>
+    /// <returns></returns>
+    public void BuildWireframeFromHeights_VEB(TerrainPointData[,] heightmap)
+    {
+        // Basic height map set up
+        int vertWidth =
+            Mathf.CeilToInt(
+                heightmap.GetLength(0) / (float)pointToVertRatio
+            );
+
+        int vertHeight =
+            Mathf.CeilToInt(
+                heightmap.GetLength(1) / (float)pointToVertRatio
+            );
+
+        vertexMap = new Vector3[vertWidth, vertHeight];
+
+        BuildVerts(heightmap);
+        BuildEdges();
+        BuildMatrices();
+
+    }
+
+    private void BuildVerts(TerrainPointData[,] heightmap)
+    {
+        // ---------------------
+        // Build verts
+        // --------------------- 
+        // I know I can do this mathimatically but the formula escapes me atm
+        int r = 0, c = 0;
+
+        // Loop over the height map at point ratio intervals 
+        for (int z = 0; z < heightmap.GetLength(1); z += pointToVertRatio)
+        {
+            for (int x = 0; x < heightmap.GetLength(0); x += pointToVertRatio)
+            {
+                // Place each vert
+                vertexMap[c, r] = new Vector3(
+                    x,
+                    heightmap[x, z].height,
+                    z
+                );
+                c++;
+            }
+            r++;
+            c = 0;
+        }
+    }
+
+    private void BuildEdges()
+    {
+        // ---------------------
+        // Build connections
+        // ---------------------
+        int width = vertexMap.GetLength(0);
+        int height = vertexMap.GetLength(1);
+
+        for (int z = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Vector3 current =
+                    vertexMap[x, z];
+
+                if (x + 1 < width)
+                {
+                    edges.Add(new Edge(
+                        current,
+                        vertexMap[x + 1, z]
+                    ));
+                }
+
+                if (z + 1 < height)
+                {
+                    edges.Add(new Edge(
+                        current,
+                        vertexMap[x, z + 1]
+                    ));
+                }
+
+                if (x + 1 < width &&
+                    z + 1 < height)
+                {
+                    edges.Add(new Edge(
+                        current,
+                        vertexMap[x + 1, z + 1]
+                    ));
+                }
+            }
+        }
+    }
+
+    private void BuildMatrices()
+    {
+        // ---------------------
+        // Build edgeMatrices for rendering
+        // ---------------------
+        foreach (Edge e in edges)
+        {
+            Vector3 edge = e.end - e.start;
+            float distance = edge.magnitude;
+            Vector3 midpoint =
+                (e.start + e.end) * 0.5f;
+
+            // Getting the sesired forward vector stored in a quat
+            Quaternion rot =
+                Quaternion.FromToRotation(
+                    Vector3.up,
+                    edge.normalized
+                );
+
+            Matrix4x4 matrix =
+                 Matrix4x4.TRS(
+                     midpoint,
+                     rot,
+                     new Vector3(0.2f, distance / 2, 0.2f)  // Scale
+                 );
+
+            edgeMatrices.Add(matrix);
+            RenderWireframe();
+        }
+    }
+
+    private void RenderWireframe()
+    {
+        const int batchSize = 1023;
+
+        Matrix4x4[] batch = new Matrix4x4[batchSize];
+
+        for (int i = 0; i < edgeMatrices.Count; i += batchSize)
+        {
+            int count =
+                Mathf.Min(
+                    batchSize,
+                    edgeMatrices.Count - i
+                );
+
+            for (int j = 0; j < count; j++)
+            {
+                batch[j] = edgeMatrices[i + j];
+            }
+
+            Graphics.DrawMeshInstanced(
+                cylMesh,
+                0,
+                cylMat,
+                batch,
+                count
+            );
+        }
+    }
+    #endregion
+
 }
