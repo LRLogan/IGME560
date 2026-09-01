349439f866374571a1915ca353d0edab381435bc
LRLogan <lrl2777@g.rit.edu>
2026-09-01 12:14:46 -0400

    Created standard scripts

---
+++ IGME661/IGME661_Assignment1/Assets/Scripts/SimManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+/// <summary>
+/// Controls overall pipeline behavior for the terrain gen sim
+/// </summary>
+public class SimManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
