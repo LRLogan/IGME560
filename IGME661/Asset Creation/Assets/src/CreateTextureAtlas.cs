a4f7a57608d13b07194aff88f408a120b7e3fb47
Logan Larrondo <145593140+LRLogan@users.noreply.github.com>
2026-09-01 10:02:37 -0400

    added class demos for 661

---
+++ IGME661/Asset Creation/Assets/src/CreateTextureAtlas.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+public class CreateTextureAtlas : MonoBehaviour
+{
+    // Name of directory to get files from
+    public string DirectoryName = "blocks";
+    public string OutputFileName = "../atlas.png";
+    public int ImageSize = 64;
+    public void Start()
+    {
+
+        UnityEngine.Debug.Log("Starting");
+        
+        TextureAtlas.instance.CreateAtlasComponentData(DirectoryName, OutputFileName, ImageSize);
+
+        UnityEngine.Debug.Log("Done with creation of texture atlas.");
+    }
+
+    
+}
