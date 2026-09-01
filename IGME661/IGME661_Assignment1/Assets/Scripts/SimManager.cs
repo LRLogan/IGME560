bb21dab47f000ad19a5fd4863bc651161d0ce551
LRLogan <lrl2777@g.rit.edu>
2026-09-01 12:48:48 -0400

    Atlasgen ref in sim manager

--- IGME661/IGME661_Assignment1/Assets/Scripts/SimManager.cs
+++ IGME661/IGME661_Assignment1/Assets/Scripts/SimManager.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 /// </summary>
 public class SimManager : MonoBehaviour
 {
+    [SerializeField] private AtlasGen atlasGen;
+    [SerializeField] private TerrainGen terrainGen;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Generate the texture atlas if needed
+        //atlasGen.GenerateTextureAtlas();
     }
 
     // Update is called once per frame
