1121ac637757e3f536070551f8766561eff63ed1
LRLogan <lrl2777@g.rit.edu>
2026-09-08 09:32:29 -0400

    Voronoi example init

--- IGME661/IGME661_Assignment1/Assets/Scripts/TerrainGen.cs
+++ IGME661/IGME661_Assignment1/Assets/Scripts/TerrainGen.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
 
+/* Plan:
+ * Use some of the FbM code from demo while adding in some tidbits like customization and domain warping from 560 final proj
+ * In terms of placing assets and texturing ground I can use both slope and biome data
+ * In the end for this assignment I would like to have a nice island generator
+ * Maybe even if the island size premits and there is an ideal location I can add a building 
+ */
+
 /// <summary>
 /// Core pipeline for generating procedural terrain
 /// </summary>
 public class TerrainGen : MonoBehaviour
 {
+    [SerializeField] Material atlasMat;
+    private TerrainSettings terrainSettings;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
