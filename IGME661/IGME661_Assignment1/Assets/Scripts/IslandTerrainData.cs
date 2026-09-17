b1664b4cc05827ad3f186b3d32ade35c6d2dfdc8
LRLogan <lrl2777@g.rit.edu>
2026-09-17 19:34:22 -0400

    Fixed VS skitzo attack

--- IGME661/IGME661_Assignment1/Assets/Scripts/IslandTerrainData.cs
+++ IGME661/IGME661_Assignment1/Assets/Scripts/IslandTerrainData.cs
@@ -1,58 +1,66 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class IslandTerrainData
+namespace Assets.Scripts
 {
-    public readonly int width;
-    public readonly int depth;
-
-    public readonly float[] heightMap;
-    public readonly float[] islandMask;
-
-    public readonly float worldWidth;
-    public readonly float worldDepth;
-    public readonly float maxHeight;
-
-    public IslandTerrainData(
-        int width,
-        int depth,
-        float worldWidth,
-        float worldDepth,
-        float maxHeight)
+    internal class IslandTerrainData
     {
-        if (width < 2)
-            throw new ArgumentException("Width must be at least 2.");
+        public readonly int width;
+        public readonly int depth;
 
-        if (depth < 2)
-            throw new ArgumentException("Depth must be at least 2.");
+        public readonly float[] heightMap;
+        public readonly float[] islandMask;
 
-        this.width = width;
-        this.depth = depth;
+        public readonly float worldWidth;
+        public readonly float worldDepth;
+        public readonly float maxHeight;
 
-        this.worldWidth = worldWidth;
-        this.worldDepth = worldDepth;
-        this.maxHeight = maxHeight;
+        public IslandTerrainData(
+            int width,
+            int depth,
+            float worldWidth,
+            float worldDepth,
+            float maxHeight)
+        {
+            if (width < 2)
+                throw new ArgumentException("Width must be at least 2.");
 
-        heightMap = new float[width * depth];
-        islandMask = new float[width * depth];
-    }
+            if (depth < 2)
+                throw new ArgumentException("Depth must be at least 2.");
 
-    public int GetIndex(int x, int z)
-    {
-        return x * depth + z;
-    }
+            this.width = width;
+            this.depth = depth;
 
-    public float GetHeight(int x, int z)
-    {
-        return heightMap[GetIndex(x, z)];
-    }
+            this.worldWidth = worldWidth;
+            this.worldDepth = worldDepth;
+            this.maxHeight = maxHeight;
 
-    public void SetHeight(int x, int z, float value)
-    {
-        heightMap[GetIndex(x, z)] = value;
-    }
+            heightMap = new float[width * depth];
+            islandMask = new float[width * depth];
+        }
 
-    public void SetIslandMask(int x, int z, float value)
-    {
-        islandMask[GetIndex(x, z)] = value;
+        public int GetIndex(int x, int z)
+        {
+            return x * depth + z;
+        }
+
+        public float GetHeight(int x, int z)
+        {
+            return heightMap[GetIndex(x, z)];
+        }
+
+        public void SetHeight(int x, int z, float value)
+        {
+            heightMap[GetIndex(x, z)] = value;
+        }
+
+        public void SetIslandMask(int x, int z, float value)
+        {
+            islandMask[GetIndex(x, z)] = value;
+        }
+
     }
 }
--- IGME661/IGME661_Assignment1/Assets/Scripts/TerrainGen.cs
+++ IGME661/IGME661_Assignment1/Assets/Scripts/TerrainGen.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using Assets.Scripts;
 
 /* Current debugging plan.
  * This code has a lot of un needed stuff
